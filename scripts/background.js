d29a0ba97ec869edd02d3d5b6d464015c043dcf9
hangdra <liuhangdra@icloud.com>
2021-06-24 11:28:32 +0800

    fix video in question

--- scripts/background.js
+++ scripts/background.js
@@ -2,7 +2,7 @@
 var initSwitchStatus = false;
 var unlikeExpireInMilsecL = 30*24*60*60*1000;
 var streamPlatformHost = "www.zhihu.com";
-var platformResourceHost=[/^http(s)?:\/\/(video.zhihu.com)|(pic[0-9]*.zhimg.com)\/.*/]
+var platformResourceHost=[/^http(s)?:\/\/(www.zhihu.com\/video)|(video.zhihu.com)|(pic[0-9]*.zhimg.com)\/.*/]
 var initVoice = true;
 
 //if need voice talk
